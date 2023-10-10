81ef34c05e51514a22088a18954e80bae3196be9
Donteatpineappleonpizza <winittra_saengsroy@cmu.ac.th>
2023-10-11 02:06:21 +0700

    Do InstConclude page

--- src/pages/ConcludeInstructor/index.tsx
+++ src/pages/ConcludeInstructor/index.tsx
@@ -1,9 +1,68 @@
+import React, { useState } from "react";
 import { Navbar } from "../../component/Navbar";
+import { DoughnutChart } from "../../component/Doughnut";
+import "./index.css";
 
-const ConcludeInstructor = () => {
+const ConcludeInstructor: React.FC = () => {
+  const [isChartVisible, setChartVisible] = useState(true);
+
+  const toggleChartVisibility = () => {
+    setChartVisible(!isChartVisible);
+  };
+
   return (
     <div>
-      <Navbar />;
+      <Navbar />
+      <div className="menuFrame-container top">
+        <div className="container-2">
+          <h3>วิชา FullStack Development รหัสวิชา 26x497</h3>
+          <div className="topic-3">
+            <h6>ปีการศึกษา ภาคเรียนที่ </h6>
+            <h6>จำนวนนักศึกษาที่ลงทะเบียนเรียน :</h6>
+          </div>
+          <div className="flex">
+            <img
+              src="./src/assets/conclude.svg"
+              className="conclude-icon"
+              alt="conclude"
+            />
+            <h6>สรุปคะแนนประเมินวัตถุประสงค์รายวิชา (CSO) </h6>
+          </div>
+          <div className="toggleconcludeChart" onClick={toggleChartVisibility}>
+            <h6>แผนภาพสรุปผลการประเมินและจำนวนนักศึกษา</h6>
+          </div>
+          <div
+            className="concludeChart"
+            style={{ display: isChartVisible ? "block" : "none" }}
+          >
+            {isChartVisible && (
+              <>
+                <div className="CSOnamechart">
+                  <div className="CSO">
+                    <h4>CSO1 </h4>
+                  </div>
+                  <p>cso 1 name</p>
+                </div>
+
+                <div className="DoughnutChart">
+                  <DoughnutChart />
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="container-2">
+        <div className="flex">
+          <img
+            src="./src/assets/conclude.svg"
+            className="conclude-icon"
+            alt="conclude"
+          />
+          <h6>สรุปคะแนนประเมินวัตถุประสงค์การเรียนรู้ของนักศึกษา (SO) </h6>
+        </div>
+      </div>
     </div>
   );
 };
